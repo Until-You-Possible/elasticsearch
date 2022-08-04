edc3ffc64259ed6ece27c2ade0132851664b288f
ray <gang.wang05@sap.com>
2022-08-04 18:05:00 +0800

    feat: new structure

---
+++ backend/src/main/java/com/example/es/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.es.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "About elasticsearch interface";
+    }
+
+}
